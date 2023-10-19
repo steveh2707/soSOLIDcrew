40ba549b87796686115f61a93be39ea56a946eff
Darragh <darragh.haughey@kainos.com>
2023-10-19 14:01:05 +0100

    Commiting merge

--- src/main/java/org/kainos/ea/api/ProjectService.java
+++ src/main/java/org/kainos/ea/api/ProjectService.java
@@ -1,10 +1,16 @@
 package org.kainos.ea.api;
 
+import org.kainos.ea.cli.ProjectDeliveryEmployeeRequest;
 import org.kainos.ea.db.ProjectDao;
 
 public class ProjectService {
 
     private final ProjectDao projectDao = new ProjectDao();
 
+    public int addDeliveryEmployeeToProject(int id, ProjectDeliveryEmployeeRequest projectDelivery) {
+
+        return 1;
+
+    }
 
 }
--- src/main/java/org/kainos/ea/db/ProjectDao.java
+++ src/main/java/org/kainos/ea/db/ProjectDao.java
@@ -1,10 +1,39 @@
 package org.kainos.ea.db;
 
 import org.kainos.ea.cli.ProjectDeliveryEmployee;
+import org.kainos.ea.cli.ProjectDeliveryEmployeeRequest;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ProjectDao {
 
+    public int addDeliveryEmployeeToProject(int id, ProjectDeliveryEmployeeRequest projectDelivery) throws SQLException {
+
+        Connection c = DatabaseConnector.getConnection();
+
+        String insertStatement = "INSERT INTO project_delivery_employee " +
+                "(project_id, delivery_employee_id, started_on_date) " +
+                "VALUES ";
+
+
+        for (int deliveryEmployeeId : projectDelivery.getDeliveryEmployeeIds()) {
+
+            insertStatement += "(?, ?, ?)";
+
+            PreparedStatement st = c.prepareStatement(insertStatement);
+
+            st.setInt(1, id);
+            st.setInt(2, deliveryEmployeeId);
+            st.setDate(3, projectDelivery.getStartedOnDate());
+
+            st.executeUpdate();
+
+            insertStatement += ", ";
+        }
+        return -1;
+
+    }
 
 }
